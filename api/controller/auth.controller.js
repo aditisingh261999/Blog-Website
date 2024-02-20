43945861c02c302b680377f2b0b6e32d32e503bc
Tysum <vinamraraaz@gmail.com>
2024-02-20 14:34:51 +0530

    Created signin route

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../Models/user.model.js";
 import bcryptjs from "bcryptjs";
+import jwt from "Jsonwebtoken";
 import { errorHandler } from "../utils/errorHandler.js";
 
 export const signup = async (req, res, next) => {
@@ -28,6 +29,40 @@ export const signup = async (req, res, next) => {
 
     const user = await newUser.save();
     res.json(user);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(400, "All fields are required"));
+  }
+  try {
+    const validUser = await User.findOne(email);
+    if (!validUser) {
+      throw errorHandler(errorHandler(401, "Invalid credentials"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      throw errorHandler(errorHandler(401, "Invalid credentials"));
+    }
+    // now signin the user using jwt token
+    const token = jwt.sign({ _id: validUser._id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: 3600,
+    });
+    // we dont want to show the pasword
+    const { password: pass, ...rest } = validUser._doc;
+
+    res.cookie("access_token", token, {
+      expires: new Date(Date.now() + 3600000),
+      httpOnly: true,
+    });
+    res.json({
+      success: true,
+      rest,
+    });
   } catch (error) {
     next(error);
   }
--- api/routes/auth.routes.js
+++ api/routes/auth.routes.js
@@ -3,5 +3,6 @@ import { signup } from "../controller/auth.controller.js";
 const route = express.Router();
 
 route.post("/signup", signup);
+route.post("/signin", signin);
 
 export default route;
